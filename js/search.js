cab5d769d0bb215188105c5cf6689b4fd22517d5
weikh <algoritm1202@gmail.com>
2022-09-29 20:52:21 +0500

    start project

---
+++ js/search.js
@@ -0,0 +1,17 @@
+let search = [
+    {
+        title: "1.Bakteriyalar necha xil- juda k`op ( yoki 2,5,4)",
+    },
+    {
+        title: "2.Odam miyasining necha foizi suvdan iborat-  80%",
+    },
+    {
+        title: "3.Turg`un tarozida turp tortdi- tiloldi,sirg`aluvchi,jarangli",
+    },
+    {
+        title: "4.Namangan viloyati-Chotqol,Qurama tog` tizmalari bilan o`raglan",
+    },
+    {
+        title: "5.Qarnobcho`l- Samarqandda joylashgan",
+    },
+]
